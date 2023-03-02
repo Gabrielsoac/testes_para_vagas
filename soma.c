7a6735cc1468d3cbb1308ccb167a8235cdfbef18
Gabriel Soares <gabrielsoacc@gmail.com>
2023-03-02 07:19:09 -0300

    soma de K

---
+++ soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int indice = 13, soma = 0, k = 0;
+
+    while (k < indice)
+    {
+     k = k + 1;
+     soma = soma + k;
+    }
+    
+    printf("o valor da soma e: %i", soma);
+
+    return 0;
+}
